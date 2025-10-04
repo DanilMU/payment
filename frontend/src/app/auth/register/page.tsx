4995396904ab3b11bbd902d1c01d5189d0f87a6d
DanilMU <danilmusatov3@yandex.ru>
2025-10-04 21:41:26 +0300

    Registration page

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,10 +1,11 @@
-import { RegisterForm } from "@/components/auth/register-form"
-import { Metadata } from "next"
+import type { Metadata } from 'next'
+
+import { RegisterForm } from '@/components/auth/register-form'
 
 export const metadata: Metadata = {
-    title: 'Регистрация'
+	title: 'Регистрация'
 }
 
 export default function RegisterPage() {
-    return <RegisterForm />
+	return <RegisterForm />
 }
--- frontend/src/components/auth/register-form.tsx
+++ frontend/src/components/auth/register-form.tsx
@@ -1,14 +1,115 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import z from 'zod'
+
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '../ui/form'
+import { Input } from '../ui/input'
+
 import { AuthWrapper } from './auth-wrapper'
+import { Button } from '../ui/button'
+
+const registerSchema = z.object({
+	name: z.string().min(1, { message: 'Имя обязательно' }),
+	email: z.email({ message: 'Введите корректный адрес электронной почты' }),
+	password: z
+		.string()
+		.min(6, { message: 'Пароль должен содержать хотя бы 6 символов' })
+		.max(128, { message: 'Пароль должен содержать не более 128 символов' })
+})
+
+export type RegisterFormValues = z.infer<typeof registerSchema>
 
 export function RegisterForm() {
+	const form = useForm<RegisterFormValues>({
+		resolver: zodResolver(registerSchema),
+		defaultValues: {
+			name: '',
+			email: '',
+			password: ''
+		}
+	})
+
+	const onSubmit = (values: RegisterFormValues) => {
+		console.log(values)
+	}
+
 	return (
 		<AuthWrapper
 			title='Регистрация'
 			description='Заполните форму, чтобы создать аккаунт'
-            bottomText='Уже есть аккаунт?'
-            bottomTextLink='Войти'
-            bottomLinkHref='/auth/login'
-            
-		>Content</AuthWrapper>
+			bottomText='Уже есть аккаунт?'
+			bottomTextLink='Войти'
+			bottomLinkHref='/auth/login'
+		>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className='space-y-4'
+				>
+					<FormField
+						control={form.control}
+						name='name'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Имя</FormLabel>
+								<FormControl>
+									<Input placeholder='Ваше имя' {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Почта</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='name@example.com'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name='password'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Пароль</FormLabel>
+								<FormControl>
+									<Input
+										type='password'
+										placeholder='123456'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<Button type='submit' size='lg' className='w-full'>
+						Продолжить
+					</Button>
+				</form>
+			</Form>
+		</AuthWrapper>
 	)
 }
