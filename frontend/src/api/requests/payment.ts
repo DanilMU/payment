aa403c95b4d8d82db190d5d9b8a29d0c338db1a4
DanilMU <danilmusatov3@yandex.ru>
2025-10-12 23:49:52 +0300

    Successful payment page

--- frontend/src/api/requests/payment.ts
+++ frontend/src/api/requests/payment.ts
@@ -1,5 +1,12 @@
-import { instance } from "../instance";
-import type { InitPaymentRequest } from "../types";
+import { api, instance } from "../instance";
+import type { InitPaymentRequest, InitPaymentResponse, PaymentDetailsResponse } from "../types";
+
+export const getPaymentById = async (id: string) =>
+    await api
+        .get<PaymentDetailsResponse>(`/payment/${id}`)
+        .then(res => res.data)
 
 export const initPayment = async (data: InitPaymentRequest) =>
-    await instance.post('/payment/init', data).then(res => res.data)
+    await instance
+        .post<InitPaymentResponse>('/payment/init', data)
+        .then(res => res.data)
